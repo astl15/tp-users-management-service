209bf4e97c6f9bb16d6e27c58abb1a1754c6c53e
astl15 <stelea.andrei@gmail.com>
2022-10-22 22:30:52 +0300

    Add project structure

---
+++ README.md
@@ -0,0 +1,1 @@
+# tp-users-management-service
---
+++ tp-users-management-exposition/src/main/java/com/astl/tp/TpUsersManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.astl.tp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TpUsersManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TpUsersManagementServiceApplication.class, args);
+	}
+
+}
